2421802ba3d52f9da02e2360c3af41cf89550757
Navneet Shahi <navneetshahi345@gmail.com>
2025-03-17 20:40:43 +0530

    build(deps): update Header.jsx, package.json and yarn.lock

--- app/dashboard/_components/Header.jsx
+++ app/dashboard/_components/Header.jsx
@@ -1,16 +1,43 @@
+"use client";
 import { UserButton } from "@clerk/nextjs";
 import Image from "next/image";
+import { usePathname } from "next/navigation";
 import React from "react";
 
 const Header = () => {
+  const path = usePathname();
   return (
-    <div>
-      <Image src={"/logo.svg"} width={80} height={80} />
-      <ul className="">
-        <li className="">Dashboard</li>
-        <li className="">Questions</li>
-        <li className="">Upgrade</li>
-        <li className="">How it works?</li>
+    <div className="flex justify-between items-center p-4 bg-secondary shadow-md">
+      <Image src={"/logo.svg"} width={60} height={60} />
+      <ul className="hidden md:flex gap-6">
+        <li
+          className={`hover:text-purple-700 hover:font-bold transition-all cursor-pointer ${
+            path == "/dashboard" && "text-purple-700 font-bold"
+          }`}
+        >
+          Dashboard
+        </li>
+        <li
+          className={`hover:text-purple-700 hover:font-bold transition-all cursor-pointer ${
+            path == "/questions" && "text-purple-700 font-bold"
+          }`}
+        >
+          Questions
+        </li>
+        <li
+          className={`hover:text-purple-700 hover:font-bold transition-all cursor-pointer ${
+            path == "/upgrade" && "text-purple-700 font-bold"
+          }`}
+        >
+          Upgrade
+        </li>
+        <li
+          className={`hover:text-purple-700 hover:font-bold transition-all cursor-pointer ${
+            path == "/about" && "text-purple-700 font-bold"
+          }`}
+        >
+          How it works?
+        </li>
       </ul>
       <UserButton />
     </div>
