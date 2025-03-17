12044658b790340e6d6dddbabdc012c257078a37
Navneet Shahi <navneetshahi345@gmail.com>
2025-03-18 00:06:09 +0530

    update drizzle.config.ts

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,8 +2,7 @@ import "dotenv/config";
 import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
-  out: "./drizzle",
-  schema: "./utils/schema.js",
+  schema: "./utils/schema.ts",
   dialect: "postgresql",
   dbCredentials: {
     url: process.env.NEXT_PUBLIC_DATABASE_URL!,
