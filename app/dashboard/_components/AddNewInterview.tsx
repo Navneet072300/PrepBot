1c4683ed86beac87c20f38f7d9bd3e3b7d17ac39
Navneet Shahi <navneetshahi345@gmail.com>
2025-03-18 20:37:01 +0530

    update AddNewInterview.tsx

--- app/dashboard/_components/AddNewInterview.tsx
+++ app/dashboard/_components/AddNewInterview.tsx
@@ -8,9 +8,20 @@ import {
   DialogTitle,
 } from "../../../components/ui/dialog";
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
 
 const AddNewInterview = () => {
   const [openDialog, setOpenDialog] = useState(false);
+  const [jobDescription, setJobDescription] = useState<string>("");
+  const [jobPosition, setJobPosition] = useState<string>("");
+  const [jobExperience, setJobExperience] = useState<string>("");
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const onSubmit = (e: any) => {
+    e.preventDefault();
+  };
+
   return (
     <div>
       <div
@@ -20,15 +31,53 @@ const AddNewInterview = () => {
         <h2 className="font-bold text-lg  text-center">+ Add New</h2>
       </div>
       <Dialog open={openDialog}>
-        <DialogContent>
+        <DialogContent className="max-w-2xl">
           <DialogHeader>
-            <DialogTitle>Are you absolutely sure?</DialogTitle>
+            <DialogTitle className="text-2xl">
+              Tell us more about your Job Interview
+            </DialogTitle>
             <DialogDescription>
-              This action cannot be undone. This will permanently delete your
-              account and remove your data from our servers.
+              <form onSubmit={onSubmit}>
+                <div className="">
+                  <h2 className="">
+                    Add details about you job role, description and years of
+                    experience
+                  </h2>
+                  <div className="mt-7 my-4">
+                    <label>Job Role</label>
+                    <Input
+                      placeholder="e.g. Full Stack Developer"
+                      required
+                      onChange={(e) => setJobPosition(e.target.value)}
+                    />
+                  </div>
+                  <div className=" my-4">
+                    <label>Job Description</label>
+                    <Textarea
+                      placeholder="e.g. React JS, Javascript, HTML, JAVA etc."
+                      required
+                      onChange={(e) => setJobDescription(e.target.value)}
+                    />
+                  </div>
+                  <div className=" my-4">
+                    <label>Years of Experience</label>
+                    <Input
+                      placeholder="e.g. 5"
+                      type="number"
+                      max={"40"}
+                      required
+                      onChange={(e) => setJobExperience(e.target.value)}
+                    />
+                  </div>
+                </div>
+              </form>
               <div className="flex gap-5 justify-end">
-                <Button>Start Interview</Button>
-                <Button variant={"ghost"} onClick={() => setOpenDialog(false)}>
+                <Button type="submit">Start Interview</Button>
+                <Button
+                  type="button"
+                  variant={"ghost"}
+                  onClick={() => setOpenDialog(false)}
+                >
                   Cancel
                 </Button>
               </div>
