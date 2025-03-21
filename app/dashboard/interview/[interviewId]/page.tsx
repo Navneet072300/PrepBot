81775321f947f30c92e3c90e09858240d2d1f60f
Navneet Shahi <navneetshahi345@gmail.com>
2025-03-21 23:21:48 +0530

    update page.tsx

--- app/dashboard/interview/[interviewId]/page.tsx
+++ app/dashboard/interview/[interviewId]/page.tsx
@@ -5,7 +5,7 @@ import { Button } from "@/components/ui/button";
 import { db } from "@/utils/db";
 import { mockInterview } from "@/utils/schema";
 import { eq } from "drizzle-orm";
-import { WebcamIcon } from "lucide-react";
+import { Lightbulb, WebcamIcon } from "lucide-react";
 import React, { useEffect, useState } from "react";
 import Webcam from "react-webcam";
 
@@ -48,19 +48,27 @@ const Interview = ({ params }: { params: { interviewId: string } }) => {
         Let&apos;s get started with the mock interview!
       </h2>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
-        <div className="flex flex-col my-5 gap-5 p-5 rounded-l-lg border">
-          <h2 className="text-lg">
-            <strong>Job Role:</strong>
-            {interviewData?.jobPosition}
-          </h2>
-          <h2 className="text-lg">
-            <strong>Job Description:</strong>
-            {interviewData?.jobDes}
-          </h2>
-          <h2 className="text-lg">
-            <strong>Years of Experience:</strong>
-            {interviewData?.jobExp}
-          </h2>
+        <div className="flex flex-col my-5 gap-5">
+          <div className="flex flex-col mt-2 p-5 rounded-lg border gap-5">
+            <h2 className="text-lg">
+              <strong>Job Role:</strong>
+              {interviewData?.jobPosition}
+            </h2>
+            <h2 className="text-lg">
+              <strong>Job Description:</strong>
+              {interviewData?.jobDes}
+            </h2>
+            <h2 className="text-lg">
+              <strong>Years of Experience:</strong>
+              {interviewData?.jobExp}
+            </h2>
+          </div>
+          <div className="">
+            <h2 className="">
+              <Lightbulb />
+              <strong>Information</strong>
+            </h2>
+          </div>
         </div>
         <div className="">
           {webcamEnable ? (
